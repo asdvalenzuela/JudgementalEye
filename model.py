f50f3f2de44adf7629afafba039fbcc2f339e033
Hackbright Student <info@hackbrightacademy.com>
2014-10-29 14:35:18 -0700

    ranked the users; still need to finish prediction

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship, backref
+import correlation
 
 ENGINE = create_engine("sqlite:///ratings.db", echo=True)
 session = scoped_session(sessionmaker(bind=ENGINE,
@@ -29,6 +30,35 @@ class User(Base):
 
     # creates backref relationship to Rating class
     ratings = relationship("Rating")
+
+    def similarity(self, user2):
+        user_ratings = {}
+        rating_pairs = []
+        for r in self.ratings:
+            user_ratings[r.movie_id] = r.rating
+
+        for r in user2.ratings:
+            if r.movie_id in user_ratings:
+                rating_pairs.append((r.rating, user_ratings[r.movie_id]))
+
+        if rating_pairs:
+            return correlation.pearson(rating_pairs)
+        else:
+            return 0.0
+
+    def ranked_users(self, other_users):
+        rankings = []
+        for u in other_users:
+            pearson_coeff = self.similarity(u)
+            rankings.append((pearson_coeff, u.id))
+        return sorted(rankings[0])
+
+    def make_prediction(self, other_users, movie):
+        top_user = ranked_users(other_users)
+        for rating in top_user.ratings:
+
+
+
 
 #defines movie class
 class Movie(Base):
@@ -57,7 +87,9 @@ class Rating(Base):
         backref=backref("ratings", order_by=id))
 
 ### End class declarations
-
+u = session.query(User).get(1)
+u2 = session.query(User).get(2)
+u3 = session.query(User).get(3)
 # def connect():
 #     global ENGINE
 #     global Session
